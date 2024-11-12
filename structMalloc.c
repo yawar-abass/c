587ecba9c2d5913af6ecc299ee6e17d3f05aa4c9
yawar-abass <bhatsharik6146@gmail.com>
2024-11-12 19:48:32 +0530

    Refactor student structure and memory allocation in main function

--- structMalloc.c
+++ structMalloc.c
@@ -1,24 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "helper.c"
 #include <string.h>
 
-struct Student
+#define SIZE 3
+
+typedef struct
 {
     char name[20];
     int rollNo;
-};
+} Student;
 
 int arr[10];
 
+void display(Student *s)
+{
+    printf(" Name: %s, Roll No: %d", s->name, s->rollNo);
+}
+
 int main()
 {
-    struct Student *s1 = (struct Student *)malloc(sizeof(struct Student));
-    strcpy(s1->name, "Test");
-    s1->rollNo = 23;
-    printf("%s", s1->name);
 
-    free(s1);
-    s1 = NULL;
+    Student *students[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        students[i] = (Student *)malloc(sizeof(Student));
+        if (students[i] == NULL)
+        {
+            perror("Failed to allocate memory");
+            exit(1);
+        }
+        students[i]->rollNo = i + 1;
+        snprintf(students[i]->name, sizeof(students[i]->name), "Student%d ", i + 1);
+
+        display(students[i]);
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        free(students[i]);
+        students[i] = NULL;
+    }
+
     return 0;
 }
