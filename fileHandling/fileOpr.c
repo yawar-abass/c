2eb87c11fd942b999d093cc8062c554ddfd794c5
yawar-abass <bhatsharik6146@gmail.com>
2025-01-21 14:53:02 +0530

    refactor: rename readfile.c to fileOpr.c and update file handling logic

--- fileHandling/fileOpr.c
+++ fileHandling/fileOpr.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     FILE *fp;
     char string[100];
-    fp = fopen("file.txt", "r");
+    fp = fopen("file.txt", "w+");
 
+    if (fp == NULL)
+    {
+        exit(1);
+    }
+
+    fputs("I am writing to file", fp);
+
+    rewind(fp);
     while (!feof(fp))
     {
         fgets(string, 100, fp);
---
+++ fileHandling/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "helper.h"
+
+// multifile programming
+
+int main()
+{
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    printArray(arr, 5);
+    return 0;
+}
